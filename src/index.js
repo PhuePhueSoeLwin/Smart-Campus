311100a1f84ae52045b9a2237a1492b908237aa5
DenialLewis <6531503151@lamduan.mfu.ac.th>
2025-02-27 10:46:50 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
+// This will render the App component inside the div with the id of "root"
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')  // This should match the ID in your HTML
+  document.getElementById('root')  // This targets the div with id "root" in index.html
 );
